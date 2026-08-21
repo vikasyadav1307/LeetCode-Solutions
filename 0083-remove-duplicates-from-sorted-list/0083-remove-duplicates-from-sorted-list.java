c2cfa6eeaf6969c28e1efa2faae70d5fc4eed1ee
Vikas Yadav <vikas.yadav.cs27@iilm.edu>
2026-08-21 10:55:38 +0530

    Linked List .javaEasy

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -0,0 +1,14 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+
+        while (cur != null) {
+            while (cur.next != null && cur.val == cur.next.val) {
+                cur.next = cur.next.next;
+            }
+            cur = cur.next;
+        }
+
+        return head;
+    }
+}
