37b3676f63f416ac1182be9840d028949e81c908
Vikas Yadav <yadavvikas1307@gmail.com>
2026-09-20 19:26:58 +0530

    String .pyEasy

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.py
@@ -1,13 +1,9 @@
 class Solution:
     def reverseDegree(self, s):
-        alphabet = "abcdefghijklmnopqrstuvwxyz"
-
         ans = 0
 
         for i in range(len(s)):
-            for j in range(26):
-                if s[i] == alphabet[j]:
-                    ans += (26 - j) * (i + 1)
-                    break
+            value = ord('z') - ord(s[i]) + 1
+            ans += value * (i + 1)
 
         return ans
